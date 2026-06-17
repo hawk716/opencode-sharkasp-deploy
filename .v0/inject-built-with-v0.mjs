6f50e645059a0ee03f4f618ea5c7fc7346ca1835
hawk716 <1.95777439e+08+hawk716@users.noreply.github.com>
2026-06-17 18:22:00 +0000

    Cleanup: Keep only essential files for SharkASP.NET deployment

--- .v0/inject-built-with-v0.mjs
+++
@@ -1,3 +0,0 @@
-// Placeholder script for v0 build integration
-// This script is called during the build process but doesn't need to do anything
-console.log('[v0] Build integration script executed');
